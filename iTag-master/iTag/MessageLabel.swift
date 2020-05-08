0002beeb5fb0e32d3bf8db65334f303d0380b9f7
David Espiritu <despiritu@csumb.edu>
2020-05-07 17:15:10 -0700

    odError

---
+++ iTag-master/iTag/MessageLabel.swift
@@ -0,0 +1,8 @@
+//
+//  MessageLabel.swift
+//  
+//
+//  Created by David Espiritu on 5/5/20.
+//
+
+import Foundation
---
+++ iTag-master/iTag/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  
+//
+//  Created by David Espiritu on 5/5/20.
+//
+
+import Foundation
