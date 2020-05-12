d2cc7b2b2335cf2dfeea33333f79c72a992a1687
David Espiritu <despiritu@csumb.edu>
2020-05-11 18:08:55 -0700

    update

--- iTag-master/iTag/iTag/PlayerViewController.swift
+++ iTag-master/iTag/iTag/PlayerViewController.swift
@@ -15,48 +15,73 @@ class PlayerViewController: UIViewController, UITableViewDataSource, UITableView
 //    @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var tableView: UITableView!
-    var players=[PFUser]()
+    var players=[PFObject]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
-
+    
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "User")
+        query.includeKey("objectId")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (players, error) in
+            if self.players != nil {
+                self.players = players!
+                self.tableView.reloadData()
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        let player = players[section]
 //        let playerCount = (player["status"] as? [PFObject]) ?? []
 //
-//
-//
-//        return playerCount.count
-        return 8
+        return players.count
     }
 //    func numberSections(in tableView: UITableView) -> Int {
 //
 //        return players.count
 //
 //    }
-    let usernames = ["user1", "user2", "david", "admin", "zen", "latlongtest", "john", "test2"]
+//    let usernames = ["user1", "user2", "david", "admin", "zen", "latlongtest", "john", "test2"]
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
+       print("Error crap")
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell") as! PlayerCell
-//        let player = players[indexPath.row]
-//        let player_ = player.username as! PFUser
-        cell.playerLabel!.text = usernames[indexPath.row]
-        if(indexPath.row==4){
-            cell.statusLabel!.text = "Current Status: IT"
-        }else{
-        cell.statusLabel!.text = "Current Status: Hiding"
-        }
+        let player = players[indexPath.row]
+//        cell.
+        cell.playerLabel!.text = player["latitude"] as? String
+        
+        //        if(indexPath.row==4){
+//            cell.statusLabel!.text = "Current Status: IT"
+//        }else{
+//        cell.statusLabel!.text = "Current Status: Hiding"
+//        }
         
         return cell
         
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           // Get the new view controller using segue.destination.
+           // Pass the selected object to the new view controller.
+           
+           let cell = sender as! UITableViewCell
+           let indexPath = tableView.indexPath(for: cell)!
+           let player = players[indexPath.row]
+           
+           let PlayerViewController = segue.destination as! PlayerViewController
+           PlayerViewController.player = player
+           tableView.deselectRow(at: indexPath, animated: true)
+       }
     
 
 
